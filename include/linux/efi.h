f619d710d38e19ee753dd6410b2998c9a649dc12
Johnny Mnemonic <jm@machine-hall.org>
2026-04-20 00:44:35 +0200

    Revert "efi: Drop unused efi_range_is_wc() function"
    ia64 is still using this in `arch/ia64/include/asm/fb.h`, so keeping that.
    
    This reverts commit 77dd8adabbc8ff845177b460de48b9d2cd579966.
    
    Signed-off-by: Johnny Mnemonic <jm@machine-hall.org>
    Signed-off-by: Tomas Glozar <tglozar@gmail.com>

--- include/linux/efi.h
+++ include/linux/efi.h
@@ -836,6 +836,27 @@ extern int __init parse_efi_signature_list(
 	const void *data, size_t size,
 	efi_element_handler_t (*get_handler_for_guid)(const efi_guid_t *));
 
+/**
+ * efi_range_is_wc - check the WC bit on an address range
+ * @start: starting kvirt address
+ * @len: length of range
+ *
+ * Consult the EFI memory map and make sure it's ok to set this range WC.
+ * Returns true or false.
+ */
+static inline int efi_range_is_wc(unsigned long start, unsigned long len)
+{
+	unsigned long i;
+
+	for (i = 0; i < len; i += (1UL << EFI_PAGE_SHIFT)) {
+		unsigned long paddr = __pa(start + i);
+		if (!(efi_mem_attributes(paddr) & EFI_MEMORY_WC))
+			return 0;
+	}
+	/* The range checked out */
+	return 1;
+}
+
 #ifdef CONFIG_EFI_PCDP
 extern int __init efi_setup_pcdp_console(char *);
 #endif
